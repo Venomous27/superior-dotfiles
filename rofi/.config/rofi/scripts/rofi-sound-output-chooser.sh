8fab9cf1bd76895aac8bd2ff5caf443f92358bfd
Venomous27 <venomousrudra27@gmail.com>
2026-07-07 23:45:18 +0530

    x

---
+++ rofi/.config/rofi/scripts/rofi-sound-output-chooser.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+declare -A MAP
+
+while IFS=$'\t' read -r name desc; do
+    MAP["$desc"]="$name"
+done < <(
+    pactl list sinks | awk '
+        /^Sink #/ {name=""; desc=""}
+        /Name:/ {sub(/^[[:space:]]*Name:[[:space:]]*/, ""); name=$0}
+        /Description:/ {
+            sub(/^[[:space:]]*Description:[[:space:]]*/, "")
+            desc=$0
+            print name "\t" desc
+        }'
+)
+
+choice=$(printf "%s\n" "${!MAP[@]}" | sort | rofi -dmenu \
+    -theme /home/venom/dotfiles/rofi/.config/rofi/themes/rofi-sound-output-chooser.rasi \
+    -p "󰓃 Output")
+[ -z "$choice" ] && exit
+
+pactl set-default-sink "${MAP[$choice]}"
+
+notify-send "Default output" "$choice"
